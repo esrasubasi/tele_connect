9b09317450f745a9616693a5fa2ebed596b7e690
Esra Subaşı <esrasubasi01@gmail.com>
2023-08-11 10:59:47 +0300

    mesaj alma ve gönderme özellikleri eklendi

--- tele_connect/lib/main.dart
+++ tele_connect/lib/main.dart
@@ -1,115 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:readsms/readsms.dart';
+import 'package:sms_receiver/sms_receiver.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyApp> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
+  final _plugin = Readsms();
+  String sms = '';
+  String sender = '';
+  String time = '';
+  String? _textContent = 'Mesajlar bekleniyor...';
+  SmsReceiver? _smsReceiver;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _smsReceiver = SmsReceiver(onSmsReceived, onTimeout: onTimeout);
+    _startListening();
+
+    getPermission().then((value) {
+      if (value == true) {
+        _plugin.read();
+        _plugin.smsStream.listen((event) {
+          setState(() {
+            sms = event.body;
+            sender = event.sender;
+            time = event.timeReceived.toString();
+            _sendSMS();
+          });
+        });
+      }
     });
   }
 
+  Future<bool> getPermission() async {
+    if (await Permission.sms.status == PermissionStatus.granted) {
+      return true;
+    } else {
+      if (await Permission.sms.request() == PermissionStatus.granted) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  void onSmsReceived(String? message) {
+    setState(() {
+      _textContent = message;
+    });
+  }
+
+  void onTimeout() {
+    setState(() {
+      _textContent = 'Zaman doldu!!!';
+    });
+  }
+
+  void _startListening() async {
+    if (_smsReceiver == null) return;
+    await _smsReceiver?.startListening();
+    setState(() {
+      _textContent = 'Mesajlar bekleniyor...';
+    });
+  }
+
+  void _stopListening() async {
+    if (_smsReceiver == null) return;
+    await _smsReceiver?.stopListening();
+    setState(() {
+      _textContent = 'Dinleyici Durdu.';
+    });
+  }
+
+  List<String> recipients = ["+905313562627"];
+
+  void _sendSMS() async {
+    await sendSMS(message: sms, recipients: recipients, sendDirect: true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tele Connect'),
+        ),
+        body: Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              alignment: Alignment.center,
+              child: Text(_textContent ?? 'boş'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: _startListening,
+              child: const Text('Tekrar Dinle'),
+            ),
+            ElevatedButton(
+              onPressed: _stopListening,
+              child: const Text('Dinleyiciyi Durdur'),
+            ),
+            const Divider(),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Yeni mesaj alındı: $sms'),
+                  Text('Gönderen: $sender'),
+                  Text('SMS Zamanı: $time'),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
