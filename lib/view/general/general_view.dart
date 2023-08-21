60b10aa4ac23c70d5b4873fd14b3438af69fcc1c
Ege Becin <egebecin2@gmail.com>
2023-08-21 11:27:49 +0300

    birleşti sadece maiş kaldı

--- lib/view/general/general_view.dart
+++ lib/view/general/general_view.dart
@@ -34,7 +34,6 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   final GeneralViewModel viewModel = GeneralViewModel();
   final SMSReadViewModel readModel = SMSReadViewModel();
-  List<String> telno = [];
   void initState() {
     readModel.SavedSend = SavedSender;
     super.initState();
@@ -71,92 +70,94 @@ class _HomeState extends State<Home> {
       ),
       backgroundColor: ColorConstant.MAIN_COLOR,
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Switch(
-              value: isOn,
-              onChanged: (value) {
-                swiprovider.toggleIsOn(value);
-              },
-              activeColor: ColorConstant.MAIN_COLOR2,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            StreamBuilder<QuerySnapshot>(
-              stream: viewModel.personsStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                }
-
-                if (!snapshot.hasData) {
-                  return Center(child: CircularProgressIndicator());
-                }
-
-                final persons = snapshot.data!.docs.map((doc) {
-                  final data = doc.data() as Map<String, dynamic>;
-                  return Person(
-                    personName: data['PersonName'],
-                    personNumber: data['PersonNumber'],
-                    personEmail: data['PersonEmail'],
-                    personSelect: data['PersonSelect'] ?? false,
-                  );
-                }).toList();
-
-                return Container(
-                  height: 300,
-                  width: 500,
-                  margin: const EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(color: ColorConstant.MAIN_COLORB54, width: 3),
-                  ),
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.vertical,
-                    child: Column(
-                      children: [for (final person in persons) PadCheckB(person), Text(recipients.toString())],
-                    ),
-                  ),
-                );
-              },
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            SafeArea(
-              child: Row(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        RouteHelper.push(context, SendView());
-                      },
-                      icon: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Icon(Icons.message),
-                      ),
-                      iconSize: 100,
-                      color: ColorConstant.MAIN_COLOR2),
-                  SizedBox(
-                    width: 70,
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        RouteHelper.push(context, PersonView());
-                      },
-                      icon: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Icon(Icons.add_circle),
-                      ),
-                      iconSize: 100,
-                      color: ColorConstant.MAIN_COLOR2),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
               ),
-            ),
-          ],
+              Switch(
+                value: isOn,
+                onChanged: (value) {
+                  swiprovider.toggleIsOn(value);
+                },
+                activeColor: ColorConstant.MAIN_COLOR2,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              StreamBuilder<QuerySnapshot>(
+                stream: viewModel.personsStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+
+                  if (!snapshot.hasData) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  final persons = snapshot.data!.docs.map((doc) {
+                    final data = doc.data() as Map<String, dynamic>;
+                    return Person(
+                      personName: data['PersonName'],
+                      personNumber: data['PersonNumber'],
+                      personEmail: data['PersonEmail'],
+                      personSelect: data['PersonSelect'] ?? false,
+                    );
+                  }).toList();
+
+                  return Container(
+                    height: 300,
+                    width: 500,
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(color: ColorConstant.MAIN_COLORB54, width: 3),
+                    ),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        children: [for (final person in persons) PadCheckB(person), Text(recipients.toString())],
+                      ),
+                    ),
+                  );
+                },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              SafeArea(
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          RouteHelper.push(context, SendView());
+                        },
+                        icon: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Icon(Icons.message),
+                        ),
+                        iconSize: 100,
+                        color: ColorConstant.MAIN_COLOR2),
+                    SizedBox(
+                      width: 70,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          RouteHelper.push(context, PersonView());
+                        },
+                        icon: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Icon(Icons.add_circle),
+                        ),
+                        iconSize: 100,
+                        color: ColorConstant.MAIN_COLOR2),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
